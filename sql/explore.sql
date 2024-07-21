3b872fc5ad7a716643d61496b0819817af2892ce
yt Chen <yutongc30@gmail.com>
2024-07-21 16:01:39 +0800

    資料表備份

---
+++ sql/explore.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-07-20 13:25:26
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `project20240714`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `explore`
+--
+
+CREATE TABLE `explore` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `img` text NOT NULL,
+  `title` text NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `explore`
+--
+
+INSERT INTO `explore` (`id`, `img`, `title`, `text`) VALUES
+(1, '03.jpg', '66', '666'),
+(2, '02.jpg', 'title2', 'text2'),
+(3, '03.jpg', 'title3', 'text3'),
+(4, '02.jpg', '9999', '9999'),
+(5, '03.jpg', '555', '99'),
+(6, '01.jpg', '99', '777');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `explore`
+--
+ALTER TABLE `explore`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `explore`
+--
+ALTER TABLE `explore`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
