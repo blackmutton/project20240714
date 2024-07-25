e156fa5f5ecb0a549622ee79357eb0c8b4e8eadc
yt Chen <yutongc30@gmail.com>
2024-07-26 01:33:14 +0800

    修正reg()無作用的bug

--- front/reg.php
+++ front/reg.php
@@ -1,31 +1,35 @@
-<fieldset>
+<style>
+    td{
+    text-align-last:justify
+}
+</style>
+<fieldset  class="text-center">
     <legend>會員註冊</legend>
     <div style="color:red">請設定您要註冊的帳號及密碼</div>
-    <table>
+    <table class="mx-auto">
         <tr>
-            <td>登入帳號</td>
+            <td>登入帳號:</td>
             <td><input type="text" name="acc" id="acc"></td>
         </tr>
         <tr>
-            <td>登入密碼</td>
+            <td>登入密碼:</td>
             <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
-            <td>再次確認密碼</td>
+            <td>再次確認密碼:</td>
             <td><input type="password" name="pw2" id="pw2"></td>
         </tr>
         <tr>
-            <td>信箱(忘記密碼時使用)</td>
+            <td>信箱(忘記密碼時使用):</td>
             <td><input type="email" name="email" id="email"></td>
         </tr>
-        <tr>
+        <tr>   
             <td>
-                <button onclick="reg()">註冊</button>
-                <button onclick="clean()">清除</button>
-            </td>
-            <td></td>
-        </tr>
-    </table>
+                <button onclick="reg()" class="btn btn-primary">註冊</button>
+                <button onclick="clean()" class="btn btn-warning">清除</button>
+            </td>          
+            </tr>
+        </table>
 </fieldset>
 <script>
 function reg(){
@@ -36,7 +40,7 @@ function reg(){
         email:$("#email").val(),
     }
 
-    if(user.acc=''||user.pwd=''||user.pw2=''||user.email=''){
+    if(user.acc==''||user.pwd==''||user.pw2==''||user.email==''){
         alert("不可空白")
     }else if(user.pwd!=user.pw2){
         alert("密碼欄位不一致")
