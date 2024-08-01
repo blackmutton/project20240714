51364cf24086495fccd5422e51bb185eb722f915
yt Chen <yutongc30@gmail.com>
2024-08-01 10:53:42 +0800

    解決ajax.js(刪除資料用)中使用dataType:json會報錯的疑惑

--- api/del_all.php
+++ api/del_all.php
@@ -1,12 +1,15 @@
 <?php
 include_once "db.php";
-dd($_POST);
-$db=${ucfirst($_POST['table'])};
-dd($db);
+// 因為回傳時會抓列印出來的資料，故需要註解掉dd()
+// dd($_POST);
+$db = ${ucfirst($_POST['table'])};
+// dd($db);
 
-if(isset($_POST['id'])&& isset($_POST['table'])){
+if (isset($_POST['id']) && isset($_POST['table'])) {
     $db->del("{$_POST['id']}");
 }
-
-
-
+// 為了避免$.ajax的dataType因抓不到資料導致錯誤，故在這邊設定一個回傳的json資料
+echo json_encode([
+    'msg' => 'ok',
+    'id' => $_POST['id']
+]);
--- js/ajax.js
+++ js/ajax.js
@@ -1,34 +1,35 @@
 // 刪除user以外的ajax，用在後台
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const del=$(".del");
-    del.click(function(){
-     console.log('this', this);
-     console.log('$(this)', $(this));
+    const del = $(".del");
+    del.click(function () {
+        //  console.log('this', this);
+        //  console.log('$(this)', $(this));
 
-     let nowBtn = $(this);
-     const id=nowBtn.data("id")
-     const table=nowBtn.data("do")
-     const data={'id':id,'table':table}
+        let nowBtn = $(this);
+        const id = nowBtn.data("id")
+        const table = nowBtn.data("do")
+        const data = { 'id': id, 'table': table }
 
-     console.log('id',id)
-     console.log('table',table)
+        //  console.log('id',id)
+        //  console.log('table',table)
 
-     // 因為include到admin.php，因此層級也是從admin.php開始算
-     let url="./api/del_all.php";
+        // 因為include到admin.php，因此層級也是從admin.php開始算
+        let url = "./api/del_all.php";
 
-     $.ajax({
-         type:"post",
-         url:url,
-         data:data,
-         // dataType:"json",
-         success: function(res){
-            //  console.log('res',res)
-             window.location.reload();
+        $.ajax({
+            type: "post",
+            url: url,
+            data: data,
+            //  由於delete一般沒有回傳值，所以指定回傳的dataType便會導致錯誤，為了避免錯誤需要另外在api中設定一個json資料回傳
+            dataType: "json",
+            success: function (res) {
+                console.log('res', res)
+                window.location.reload();
 
-         }, error: function(err){
-             console.log("err: ", err)
-         }
-         })
-     })
+            }, error: function (err) {
+                console.log("err: ", err)
+            }
+        })
+    })
 })
